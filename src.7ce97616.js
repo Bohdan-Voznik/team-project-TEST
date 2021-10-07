6cc1ed257bdb476550ba699f053ff12cdc2923f6
Bohdan-Voznik <87948538+Bohdan-Voznik@users.noreply.github.com>
2021-10-07 18:41:23 +0000

    Deploying to gh-pages from @ Bohdan-Voznik/team-project-TEST@d6555ae588fff7b61852fbd53cc2dcbd30646ce5 🚀

--- src.7ce97616.js
+++ src.7ce97616.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-TEST/src.aeb79d9b.js.map
+//# sourceMappingURL=/team-project-TEST/src.7ce97616.js.map
